8093e791b951cd88d6f6a892864b405db4b682b0
DJSmith11 <142458119+DJSmith11@users.noreply.github.com>
2024-01-25 18:38:46 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,17 @@
+    }
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+        console.log("Random Number: " + item);
+main();
+    });
+}
+    for (let i = 0; i < 10; i++) {
+    return data;
+    const data = generateRandomData();
+    data.forEach(item => {
+    const data = [];
+
+
+}
+function generateRandomData() {
+function main() {
